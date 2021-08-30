48dbe2ddf3b3b7648f6cd247c9d0abb250e73929
KriszTuri <turi.krisztina1999@gmail.com>
2021-08-30 17:37:31 +0200

    base of moveAnts somewhat ready

--- src/main/java/com/codecool/ants/geometry/Colony.java
+++ src/main/java/com/codecool/ants/geometry/Colony.java
@@ -61,52 +61,85 @@ public class Colony {
         return new int[]{randomCoordinate1, randomCoordinate2};
     }
 
+    public int[] antSpecificGeneratedCoordinates(String movePattern, int[] currentCoordinates){
+        int[] coordinates;
+        switch (movePattern) {
+            case "patrol":
+                coordinates = new int[]{0, 0};
+                break;
+            case "approachQueen":
+                //TODO: check x, move x, if it's = Queen x -> check y, move y
+                coordinates = new int[]{1, 1};
+                break;
+            case "NORTH":
+                coordinates = new int[]{currentCoordinates[0]+1, currentCoordinates[1]};
+                break;
+            case "EAST":
+                coordinates = new int[]{currentCoordinates[0], currentCoordinates[1]+1};
+                break;
+            case "WEST":
+                coordinates = new int[]{currentCoordinates[0], currentCoordinates[1]-1};
+                break;
+            case "SOUTH":
+                coordinates = new int[]{currentCoordinates[0]-1, currentCoordinates[1]};
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + movePattern);
+        }
+        return coordinates;
+    }
+
     /**Generates soldiers, drones and workers, places them in the colony array.**/
     public void generateAnts(int numOfSoldiers, int numOfDrones, int numOfWorkers){
         for(int i=0; i<numOfSoldiers; i++){
             Soldiers soldier = new Soldiers();
             soldier.setName("Soldier nr. "+(i+1));
             soldier.setMovePattern("patrol");
-            placeAnt(soldier, soldier.letterForDisplay);
+            placeAnt(soldier, soldier.letterForDisplay, "random");
         }
 
         for(int i=0; i<numOfDrones; i++){
             Drones drone = new Drones();
             drone.setName("Drone nr. "+(i+1));
             drone.setMovePattern("approachQueen");
-            placeAnt(drone, drone.letterForDisplay);
+            placeAnt(drone, drone.letterForDisplay, "random");
         }
 
         for(int i=0; i<numOfWorkers; i++){
             Workers worker = new Workers();
             worker.setName("Worker nr. "+(i+1));
             worker.setMovePattern();
-            placeAnt(worker, worker.letterForDisplay);
+            placeAnt(worker, worker.letterForDisplay, "random");
         }
     }
 
     /**Checks generated coordinate - if it's already occupied, generates a new one and checks that one as well**/
-    private void placeAnt(Ants ant, String letterForDisplay) {
-            int[] generatedCoordinates = randomGeneratedCoordinates();
-            boolean isCoordinateTaken = isCoordinateTaken(generatedCoordinates);
-            if(!isCoordinateTaken){
+    private void placeAnt(Ants ant, String letterForDisplay, String methodOfPlacement) {
+        int[] generatedCoordinates = new int[0];    
+
+        if(methodOfPlacement.equals("random")){
+                generatedCoordinates = randomGeneratedCoordinates();
+        }
+
+        boolean isCoordinateTaken = isCoordinateTaken(generatedCoordinates);
+        if(!isCoordinateTaken){
                 colony[generatedCoordinates[0]][generatedCoordinates[1]] = letterForDisplay;
                 Position position = new Position(generatedCoordinates[0], generatedCoordinates[1], ant.name);
                 ant.setPosition(position.getPosition());
                 ant.printAntData();
-            }
-            else{
-                do{
-                    int[] newRandomCoordinates = randomGeneratedCoordinates();
-                    isCoordinateTaken = isCoordinateTaken(newRandomCoordinates);
-                    if(!isCoordinateTaken){
+        }
+        else{
+            do{
+                int[] newRandomCoordinates = randomGeneratedCoordinates();
+                isCoordinateTaken = isCoordinateTaken(newRandomCoordinates);
+                if(!isCoordinateTaken){
                         colony[newRandomCoordinates[0]][newRandomCoordinates[1]] = letterForDisplay;
                         Position position = new Position(generatedCoordinates[0], generatedCoordinates[1], ant.name);
                         ant.setPosition(position.getPosition());
                         ant.printAntData();
-                    }
-                }while(isCoordinateTaken);
-            }
+                }
+            }while(isCoordinateTaken);
+        }
     }
 
     private boolean isCoordinateTaken(int[] generatedCoordinates){
@@ -121,8 +154,8 @@ public class Colony {
 
     }
 
-    public void moveAnt(int[] oldPosition, int[] newPosition, String[] colony, Ants ant){
-
+    public void moveAnt(int[] oldPosition, int[] newPosition, Ants ant){
+        
 
     }
 
